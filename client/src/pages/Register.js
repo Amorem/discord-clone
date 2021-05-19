87ae5a61380384e49a08ed96bf23911047838201
Amorem <dev@amorem.com>
2021-05-19 13:49:48 +0200

    Register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -16,6 +16,16 @@ import toErrorMap from "utils/toErrorMap";
 import { RegisterSchema } from "validation/auth.schema";
 
 export default function Register() {
+  const history = useHistory();
+  async function handleSubmit(values, { setErrors }) {
+    try {
+      const { data } = await register(values);
+      history.push("/channels/me");
+    } catch (err) {
+      setErrors(toErrorMap(err));
+    }
+  }
+
   return (
     <Flex minHeight="100vh" width="full" align="center" justifyContent="center">
       <Box px={4} width="full" maxWidth="500px" textAlign="center">
@@ -27,43 +37,55 @@ export default function Register() {
             <Heading fontSize="24px">Welcome to Discord</Heading>
           </Box>
           <Box my={4} textAlign="left">
-            <Form>
-              <InputField
-                label="Email"
-                name="email"
-                autoComplete="email"
-                type="email"
-              />
+            <Formik
+              initialValues={{
+                email: "",
+                username: "",
+                password: "",
+              }}
+              validationSchema={RegisterSchema}
+              onSubmit={handleSubmit}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <InputField
+                    label="Email"
+                    name="email"
+                    autoComplete="email"
+                    type="email"
+                  />
 
-              <InputField label="username" name="username" />
+                  <InputField label="username" name="username" />
 
-              <InputField
-                label="password"
-                name="password"
-                autoComplete="password"
-                type="password"
-              />
+                  <InputField
+                    label="password"
+                    name="password"
+                    autoComplete="password"
+                    type="password"
+                  />
 
-              <Button
-                background="highlight.standard"
-                color="white"
-                width="full"
-                mt={4}
-                type="submit"
-                isLoading={false}
-                _hover={{ bg: "highlight.hover" }}
-                _active={{ bg: "highlight.active" }}
-                _focus={{ boxShadow: "none" }}
-              >
-                Register
-              </Button>
-              <Text mt="4">
-                Already have an account?{" "}
-                <Link as={RLink} to="/login" textColor="highlight.standard">
-                  Sign In
-                </Link>
-              </Text>
-            </Form>
+                  <Button
+                    background="highlight.standard"
+                    color="white"
+                    width="full"
+                    mt={4}
+                    type="submit"
+                    isLoading={isSubmitting}
+                    _hover={{ bg: "highlight.hover" }}
+                    _active={{ bg: "highlight.active" }}
+                    _focus={{ boxShadow: "none" }}
+                  >
+                    Register
+                  </Button>
+                  <Text mt="4">
+                    Already have an account?{" "}
+                    <Link as={RLink} to="/login" textColor="highlight.standard">
+                      Sign In
+                    </Link>
+                  </Text>
+                </Form>
+              )}
+            </Formik>
           </Box>
         </Box>
       </Box>
